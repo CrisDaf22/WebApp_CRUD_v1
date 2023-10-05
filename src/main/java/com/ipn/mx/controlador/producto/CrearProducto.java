399ac24c217a9656c58e86730d09731e82284403
Cristian <>
2023-10-05 16:09:33 -0500

    Se agregó la lectura de los elementos y el formulario para crear un nuevo producto.

--- src/main/java/com/ipn/mx/controlador/producto/CrearProducto.java
+++ src/main/java/com/ipn/mx/controlador/producto/CrearProducto.java
@@ -1,23 +1,15 @@
 package com.ipn.mx.controlador.producto;
 
-import com.ipn.mx.modelo.dao.ProductoDAO;
-import com.ipn.mx.modelo.dto.ProductoDTO;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-// urlPatterns = {} para varios url
-// value = "" cuando solo es uno
-@WebServlet(name = "ListadoProductos", value = "/ListadoProductos")
-public class ListadoProductos extends HttpServlet {
+@WebServlet(name = "CrearProducto", value = "/CrearProducto")
+public class CrearProducto extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -36,56 +28,45 @@ public class ListadoProductos extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Lista de productos</title>");
+            out.println("<title>Crear producto</title>");   
             out.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'></script>");
             out.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet'>");
             out.println("</head>");
             out.println("<body>");
             
-            out.println("<div class='container'>");
-            out.println("<h1 class='h1' style='text-align: center'>Productos</h1>");
-            out.println("<a href='CrearProducto' class='btn btn-primary'>Crear producto</a>");
-            out.println("<table class='table'>");
-            out.println("<thead class='thead-dark'>");
-            out.println("<tr>");
-            out.println("<th scope='col'>ID</th>");
-            out.println("<th scope='col'>Nombre</th>");
-            out.println("<th scope='col'>Acciones</th>");
-            out.println("</tr>");
-            out.println("</thead>");
+            out.println("<div class='container' style='width: 30%'>");
+            out.println("<h1 class='h1' style='text-align: center'>Crear</h1>");
+            out.println("<form action='ProcesarC' method='POST' enctype='multipart/form-data'>");
             
-            ProductoDAO dao = new ProductoDAO();
+            out.println("<div class='form-group'>");
+            out.println("<label for='txt_nombre'>Nombre del producto</label>");
+            out.println("<input type='text' class='form-control' id='txt_nombre' placeholder='Nombre' name='txt_nombre'>");
+            out.println("</div>");
             
-            List<ProductoDTO> productos;
-            try {
-                productos = dao.leerProductos();
-                
-                if (!productos.isEmpty()) {
-                    out.println("<tbody>");
-                    for (ProductoDTO p : productos) {
-                        out.println("<tr>");
-                        out.println("<td>" + p.getEntidad().getIdProducto() + "</td>");
-                        out.println("<td>" + p.getEntidad().getNombreProducto()+ "</td>");
-                        out.println("<td>");
-                        out.println("<a href='#' class='btn btn-outline-success'>Leer</a> &nbsp");
-                        out.println("<a href='#' class='btn btn-outline-warning'>Actualizar</a> &nbsp");
-                        out.println("<a href='#' class='btn btn-outline-info'>Eliminar</a>");
-                        out.println("</td>");
-                        out.println("</tr>");
-                    }
-                    out.println("</tbody>");
-                } else {
-                    out.println("<div class='alert alert-danger' role='alert'>No hay productos registrados.</div>");
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(ListadoProductos.class.getName()).log(Level.SEVERE, null, ex);
-                out.println("<div class='alert alert-danger' role='alert'>Error al realizar la consulta.</div>");
-            }
+            out.println("<div class='form-group'>");
+            out.println("<label for='txt_descripcion'>Descripción del producto</label>");
+            out.println("<input type='text' class='form-control' id='txt_descripcion' placeholder='Descripción' name='txt_descripcion'>");
+            out.println("</div>");
             
-            out.println("</table>");
-            out.println("<div>");
+            out.println("<div class='form-group'>");
+            out.println("<label for='txt_cantidad'>Cantidad del producto</label>");
+            out.println("<input type='number' class='form-control' id='txt_cantidad' min='10' max='100' name='txt_cantidad'>");
+            out.println("</div>");
             
+            out.println("<div class='form-group'>");
+            out.println("<label for='txt_imagen'>Imágen del producto</label>");
+            out.println("<input type='file' class='form-control' id='txt_imagen' placeholder='Sube una imágen' name='txt_imagen'>");
+            out.println("</div>");
+            
+            out.println("<br>");
+            out.println("<center><input type='submit' class='btn btn-outline-primary' text='Crear'></center>");
+            
+            // TODO: Agregar enlace para regresar al listado de productos.
+            
+            out.println("</form>");
+            out.println("</div>");
             out.println("</body>");
+            
             out.println("</html>");
         }
     }
